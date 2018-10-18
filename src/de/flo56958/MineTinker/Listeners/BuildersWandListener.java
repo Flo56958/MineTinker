daaa8aaab9b423aca5b450995db7b360a8431fa7
Flo56958 <gutsche.florian@gmail.com>
2018-10-18 14:14:42 +0200

    Disabled WorldGuard again

--- src/de/flo56958/MineTinker/Listeners/BuildersWandListener.java
+++ src/de/flo56958/MineTinker/Listeners/BuildersWandListener.java
@@ -1,10 +1,9 @@
 package de.flo56958.MineTinker.Listeners;
 
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+//import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import de.flo56958.MineTinker.Data.Lists;
 import de.flo56958.MineTinker.Main;
 import de.flo56958.MineTinker.Utilities.PlayerInfo;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -16,7 +15,6 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
 public class BuildersWandListener implements Listener {
@@ -136,7 +134,7 @@ public class BuildersWandListener implements Listener {
                                             break loop;
                                         }
 
-                                        boolean canBuild = true;
+                                        /*boolean canBuild = true;
                                         Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
                                         // WorldGuard may not be loaded
                                         if (plugin instanceof WorldGuardPlugin) {
@@ -144,9 +142,11 @@ public class BuildersWandListener implements Listener {
 
                                             canBuild = ((WorldGuardPlugin) plugin).canBuild(p, b.getWorld().getBlockAt(loc));
                                         }
-                                        if (canBuild) {
-                                            b.getWorld().getBlockAt(loc).setType(current.getType());
-                                        } else { continue; }
+                                        if (canBuild) { */
+
+                                        b.getWorld().getBlockAt(loc).setType(current.getType());
+
+                                        //} else { continue; }
 
                                         current.setAmount(current.getAmount() - 1);
                                         if (Main.getPlugin().getConfig().getBoolean("Builderswands.useDurability")) { //TODO: Add Modifiers to the Builderwand (Self-Repair, Reinforced, XP)
