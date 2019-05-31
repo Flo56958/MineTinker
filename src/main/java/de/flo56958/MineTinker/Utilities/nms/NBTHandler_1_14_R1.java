b215a0851163cda110e8b15a55a8fef39061d3a7
Draycia <lonelyyordle@gmail.com>
2019-05-31 14:20:08 -0700

    Various NBTHandler improvements

--- src/main/java/de/flo56958/MineTinker/Utilities/nms/NBTHandler_1_14_R1.java
+++ src/main/java/de/flo56958/MineTinker/Utilities/nms/NBTHandler_1_14_R1.java
@@ -1,5 +1,6 @@
 package de.flo56958.MineTinker.Utilities.nms;
 
+import net.minecraft.server.v1_14_R1.ItemStack;
 import net.minecraft.server.v1_14_R1.NBTBase;
 import net.minecraft.server.v1_14_R1.NBTTagCompound;
 import net.minecraft.server.v1_14_R1.NBTTagInt;
@@ -7,7 +8,6 @@ import net.minecraft.server.v1_14_R1.NBTTagList;
 import net.minecraft.server.v1_14_R1.NBTTagLong;
 import net.minecraft.server.v1_14_R1.NBTTagString;
 import org.bukkit.craftbukkit.v1_14_R1.inventory.CraftItemStack;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Arrays;
@@ -15,53 +15,55 @@ import java.util.List;
 
 class NBTHandler_1_14_R1 extends NBTHandler {
 
-    @Override
-    public int getInt(ItemStack item, String key) {
-        net.minecraft.server.v1_14_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+    private NBTBase getBase(org.bukkit.inventory.ItemStack item, String key) {
+        ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
 
         NBTTagCompound comp = nmsItem.getTag();
-        if (comp == null) return 0;
+        if (comp == null) return null;
 
-        NBTBase base = comp.get(key);
+        return comp.get(key);
+    }
+
+    private void setNumber(org.bukkit.inventory.ItemStack item, String key, NBTBase base) {
+        ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound comp = nmsItem.getTag();
+
+        if (comp == null) comp = new NBTTagCompound();
+
+        comp.set(key, base);
+        nmsItem.setTag(comp);
+
+        ItemMeta meta = CraftItemStack.getItemMeta(nmsItem);
+        item.setItemMeta(meta);
+    }
+
+    @Override
+    public int getInt(org.bukkit.inventory.ItemStack item, String key) {
+        NBTBase base = getBase(item, key);
         if (!(base instanceof NBTTagInt)) return 0;
 
         return ((NBTTagInt)base).asInt();
     }
 
     @Override
-    public long getLong(ItemStack item, String key) {
-        net.minecraft.server.v1_14_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
-
-        NBTTagCompound comp = nmsItem.getTag();
-        if (comp == null) return 0;
-
-        NBTBase base = comp.get(key);
+    public long getLong(org.bukkit.inventory.ItemStack item, String key) {
+        NBTBase base = getBase(item, key);
         if (!(base instanceof NBTTagLong)) return 0;
 
         return ((NBTTagLong)base).asLong();
     }
 
     @Override
-    public String getString(ItemStack item, String key) {
-        net.minecraft.server.v1_14_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
-
-        NBTTagCompound comp = nmsItem.getTag();
-        if (comp == null) return null;
-
-        NBTBase base = comp.get(key);
+    public String getString(org.bukkit.inventory.ItemStack item, String key) {
+        NBTBase base = getBase(item, key);
         if (!(base instanceof NBTTagString)) return null;
 
         return base.asString();
     }
 
     @Override
-    public List<String> getStringList(ItemStack item, String key) {
-        net.minecraft.server.v1_14_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
-
-        NBTTagCompound comp = nmsItem.getTag();
-        if (comp == null) return null;
-
-        NBTBase base = comp.get(key);
+    public List<String> getStringList(org.bukkit.inventory.ItemStack item, String key) {
+        NBTBase base = getBase(item, key);
         if (!(base instanceof NBTTagList)) return null;
 
         String[] values = (String[])((NBTTagList) base).toArray();
@@ -70,57 +72,27 @@ class NBTHandler_1_14_R1 extends NBTHandler {
     }
 
     @Override
-    public void setInt(ItemStack item, String key, int value) {
-        net.minecraft.server.v1_14_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
-        NBTTagCompound comp = nmsItem.getTag();
-
-        if (comp == null) comp = new NBTTagCompound();
-
-        NBTTagInt tag = new NBTTagInt(value);
-        comp.set(key, tag);
-        nmsItem.setTag(comp);
-
-        ItemMeta meta = CraftItemStack.getItemMeta(nmsItem);
-        item.setItemMeta(meta);
+    public void setInt(org.bukkit.inventory.ItemStack item, String key, int value) {
+        setNumber(item, key, new NBTTagInt(value));
     }
 
     @Override
-    public void setLong(ItemStack item, String key, long value) {
-        net.minecraft.server.v1_14_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
-        NBTTagCompound comp = nmsItem.getTag();
-
-        if (comp == null) comp = new NBTTagCompound();
-
-        NBTTagLong tag = new NBTTagLong(value);
-        comp.set(key, tag);
-        nmsItem.setTag(comp);
-
-        ItemMeta meta = CraftItemStack.getItemMeta(nmsItem);
-        item.setItemMeta(meta);
+    public void setLong(org.bukkit.inventory.ItemStack item, String key, long value) {
+        setNumber(item, key, new NBTTagLong(value));
     }
 
     @Override
-    public void setString(ItemStack item, String key, String value) {
-        net.minecraft.server.v1_14_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
-        NBTTagCompound comp = nmsItem.getTag();
-
-        if (comp == null) comp = new NBTTagCompound();
-
-        NBTTagString tag = new NBTTagString(value);
-        comp.set(key, tag);
-        nmsItem.setTag(comp);
-
-        ItemMeta meta = CraftItemStack.getItemMeta(nmsItem);
-        item.setItemMeta(meta);
+    public void setString(org.bukkit.inventory.ItemStack item, String key, String value) {
+        setNumber(item, key, new NBTTagString(value));
     }
 
     @Override
-    public void setStringList(ItemStack item, String key, String... value) {
+    public void setStringList(org.bukkit.inventory.ItemStack item, String key, String... value) {
         NBTTagList list = new NBTTagList();
 
         for (String val : value) list.add(new NBTTagString(val));
 
-        net.minecraft.server.v1_14_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+        ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
         NBTTagCompound comp = nmsItem.getTag();
 
         if (comp == null) comp = new NBTTagCompound();
@@ -133,8 +105,8 @@ class NBTHandler_1_14_R1 extends NBTHandler {
     }
 
     @Override
-    public boolean hasTag(ItemStack item, String key) {
-        net.minecraft.server.v1_14_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+    public boolean hasTag(org.bukkit.inventory.ItemStack item, String key) {
+        ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
         NBTTagCompound comp = nmsItem.getTag();
 
         if (comp == null) return false;
@@ -143,8 +115,8 @@ class NBTHandler_1_14_R1 extends NBTHandler {
     }
 
     @Override
-    public void removeTag(ItemStack item, String key) {
-        net.minecraft.server.v1_14_R1.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+    public void removeTag(org.bukkit.inventory.ItemStack item, String key) {
+        ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
         NBTTagCompound comp = nmsItem.getTag();
 
         if (comp == null) comp = new NBTTagCompound();
