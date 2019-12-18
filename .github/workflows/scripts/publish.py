e77024c1a70cb48200098e8c833bf5e6791ef364
Florian Gutsche <florian.gutsche@fau.de>
2019-12-18 19:42:35 +0100

    Migrating to Github Actions (#131)
    * Create maven.yml
    
    * Add files via upload
    
    * Update maven.yml
    
    * Update publish.py
    
    * Update installSpigot.sh
    
    * Update installSpigot.sh
    
    * Update maven.yml
    
    * Update installSpigot.sh
    
    * Update installSpigot.sh
    
    * Update installSpigot.sh
    
    * Update maven.yml
    
    * Update installSpigot.sh
    
    * Update installSpigot.sh
    
    * Update maven.yml
    
    * Delete installSpigot.sh
    
    * Create installSpigot.sh
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update publish.py
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update publish.py
    
    * Update publish.py
    
    * Update publish.py
    
    * Update publish.py
    
    * Update publish.py
    
    * Update publish.py
    
    * Delete .travis.yml
    
    * Delete installSpigot.sh

---
+++ .github/workflows/scripts/publish.py
@@ -0,0 +1,15 @@
+import sys
+import requests
+from datetime import datetime
+
+data = {}
+
+data['content'] = ' '
+print(data, flush=True)
+
+result = requests.post(sys.argv[3],
+                        data=data, 
+                        files={'upload_file': (sys.argv[2] + "_" + str(datetime.now().strftime('%Y-%m-%d_%H-%M')) + '_' + sys.argv[4][:4] + '.jar', 
+                        open(sys.argv[1], "rb").read())})
+print(result, flush=True)
+print(result.content, flush=True)
