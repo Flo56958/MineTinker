daee3352b6a526bca4bb30a2c81fe02b8317a008
Florian Gutsche <florian.gutsche@fau.de>
2019-12-18 15:36:47 +0100

    Update publish.py

--- .github/workflows/scripts/publish.py
+++ .github/workflows/scripts/publish.py
@@ -9,7 +9,7 @@ print(data, flush=True)
 
 result = requests.post(os.environ['secrets.DiscordWebHook'],
                         data=data, 
-                        files={'upload_file': (sys.argv[2] + '-' + os.environ['github.event_name'] + '-' + os.environ['github.ref'] + '.jar', 
+                        files={'upload_file': (sys.argv[2] + '-' + os.environ['github.sha'] + '.jar', 
                         open(sys.argv[1], "rb").read())})
 print(result, flush=True)
 print(result.content, flush=True)
