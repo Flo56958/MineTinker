40e868051284409fcd4035571e477b2b326115b7
Florian Gutsche <florian.gutsche@fau.de>
2019-12-18 19:29:51 +0100

    Update publish.py

--- .github/workflows/scripts/publish.py
+++ .github/workflows/scripts/publish.py
@@ -4,12 +4,12 @@ from datetime import datetime
 
 data = {}
 
-data['content'] = str(datetime.now())
+data['content'] = ' '
 print(data, flush=True)
 
 result = requests.post(sys.argv[3],
                         data=data, 
-                        files={'upload_file': (sys.argv[2] + '-' + sys.argv[4] + '.jar', 
+                        files={'upload_file': (sys.argv[2] + str(datetime.now().strftime('%Y-%m-%d/%H:%M:%S')) + '-' + sys.argv[4][:4] + '.jar', 
                         open(sys.argv[1], "rb").read())})
 print(result, flush=True)
 print(result.content, flush=True)
