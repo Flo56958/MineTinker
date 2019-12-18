6f7d89ffc52af25af5e66569c72d3242176ab491
Flo56958 <florian.gutsche@fau.de>
2019-12-18 23:34:01 +0100

    Improve CI scripts

--- .github/workflows/scripts/installSpigot.sh
+++ .github/workflows/scripts/installSpigot.sh
@@ -1,9 +1,9 @@
-if [ -d "$HOME/.m2/repository/org/spigotmc/spigot/$1-R0.1-SNAPSHOT" ]; 
+if [ -d "$HOME/.m2/repository/org/bukkit/craftbukkit/$1-R0.1-SNAPSHOT" ];
     then echo "Not compiling Spigot $1 because it is already in the maven repository!" ; exit 0;
 else echo "Compiling Spigot $1 because it is not in the maven repository!" ; 
 fi ;
 mkdir -p $HOME/spigot-build
-pushd $HOME/spigot-build
+pushd $HOME/spigot-build || exit # <- if pushd failes
 
 echo "Downloading Spigot Build Tools for minecraft version $1"
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O $HOME/spigot-build/BuildTools.jar
@@ -17,4 +17,4 @@ echo "Installing Spigot jar in Maven"
 mvn install:install-file -Dfile=$HOME/spigot-build/spigot-$1.jar -Dpackaging=jar -DpomFile=$HOME/spigot-build/Spigot/Spigot-Server/pom.xml
 echo "Installing CraftBukkit jar in Maven"
 mvn install:install-file -Dfile=$HOME/spigot-build/craftbukkit-$1.jar -Dpackaging=jar -DpomFile=$HOME/spigot-build/CraftBukkit/pom.xml
-popd
+popd || exit
--- .github/workflows/scripts/publish.py
+++ .github/workflows/scripts/publish.py
@@ -2,14 +2,21 @@ import sys
 import requests
 from datetime import datetime
 
+#Parameter syntax
+#0: script name
+#1: Target file
+#2: Published starting file name
+#3: Discord-Webhook-Address
+#4: Commit-SHA
+
 data = {}
 
-data['content'] = ' '
+data['content'] = ' ' # <- What will be written in the chat before the file
 print(data, flush=True)
 
 result = requests.post(sys.argv[3],
                         data=data, 
-                        files={'upload_file': (sys.argv[2] + "_" + str(datetime.now().strftime('%Y-%m-%d_%H-%M')) + '_' + sys.argv[4][:4] + '.jar', 
+                        files={'upload_file': (sys.argv[2] + "_" + str(datetime.now().strftime('%Y-%m-%d_%H-%M')) + '_commit_' + sys.argv[4][:6] + '.jar',
                         open(sys.argv[1], "rb").read())})
 print(result, flush=True)
 print(result.content, flush=True)
