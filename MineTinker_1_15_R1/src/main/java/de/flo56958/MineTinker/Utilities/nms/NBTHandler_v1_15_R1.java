e170e7ca7c33197d3dc7eea61580723b8254a5bd
Draycia <lonelyyordle@gmail.com>
2020-06-08 11:49:14 -0700

    Experimental playerBreakBlock changes

--- MineTinker_1_15_R1/src/main/java/de/flo56958/MineTinker/Utilities/nms/NBTHandler_v1_15_R1.java
+++ MineTinker_1_15_R1/src/main/java/de/flo56958/MineTinker/Utilities/nms/NBTHandler_v1_15_R1.java
@@ -4,16 +4,18 @@ import net.minecraft.server.v1_15_R1.*;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
-import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDropItemEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class NBTHandler_v1_15_R1 extends NBTHandler {
 
@@ -30,7 +32,33 @@ class NBTHandler_v1_15_R1 extends NBTHandler {
 
 	@Override
 	public boolean playerBreakBlock(Player player, Block block) {
-		return ((CraftPlayer) player).getHandle().playerInteractManager.breakBlock(new BlockPosition(block.getX(), block.getY(), block.getZ()));
+        ItemStack itemStack = player.getInventory().getItemInMainHand();
+        Block position = player.getLocation().getBlock();
+
+        BlockBreakEvent breakEvent = new BlockBreakEvent(block, player);
+
+        if (!breakEvent.isCancelled() && position.breakNaturally(itemStack)) {
+            Collection<ItemStack> items = position.getDrops(itemStack);
+
+            List<Item> itemEntities = items.stream().map(entry -> {
+                return (Item)player.getWorld().spawnEntity(player.getLocation(), EntityType.DROPPED_ITEM);
+            }).collect(Collectors.toList());
+
+            BlockDropItemEvent event = new BlockDropItemEvent(position, position.getState(), player, itemEntities);
+
+            if (!event.isCancelled()) {
+                for (Item item : event.getItems()) {
+                    player.getWorld().dropItemNaturally(player.getLocation(), item.getItemStack());
+                }
+            }
+
+            // TODO: drop experience
+
+            return true;
+        }
+
+        return false;
+		//return ((CraftPlayer) player).getHandle().playerInteractManager.breakBlock(new BlockPosition(block.getX(), block.getY(), block.getZ()));
 	}
 
     @Override
