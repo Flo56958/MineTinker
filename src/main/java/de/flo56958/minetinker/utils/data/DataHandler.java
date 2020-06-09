a7ff9de4bdefbecb44c0ca8a40574c27ece35e40
Flo56958 <florian.gutsche@fau.de>
2020-06-09 17:19:19 +0200

    Fix BlockBreak

--- src/main/java/de/flo56958/minetinker/utils/data/DataHandler.java
+++ src/main/java/de/flo56958/minetinker/utils/data/DataHandler.java
@@ -1,6 +1,7 @@
 package de.flo56958.minetinker.utils.data;
 
 import de.flo56958.minetinker.MineTinker;
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
@@ -129,21 +130,22 @@ public class DataHandler {
 
     public static boolean playerBreakBlock(Player player, Block block) {
         ItemStack itemStack = player.getInventory().getItemInMainHand();
-        Block position = player.getLocation().getBlock();
 
         BlockBreakEvent breakEvent = new BlockBreakEvent(block, player);
+        Bukkit.getPluginManager().callEvent(breakEvent);
 
-        if (!breakEvent.isCancelled() && position.breakNaturally(itemStack)) {
-            Collection<ItemStack> items = position.getDrops(itemStack);
+        if (!breakEvent.isCancelled() && block.breakNaturally(itemStack)) {
+            Collection<ItemStack> items = block.getDrops(itemStack);
 
             List<Item> itemEntities = items.stream().map(entry ->
                     (Item)player.getWorld().spawnEntity(player.getLocation(), EntityType.DROPPED_ITEM)).collect(Collectors.toList());
 
-            BlockDropItemEvent event = new BlockDropItemEvent(position, position.getState(), player, itemEntities);
+            BlockDropItemEvent event = new BlockDropItemEvent(block, block.getState(), player, itemEntities);
+            Bukkit.getPluginManager().callEvent(event);
 
             if (!event.isCancelled()) {
                 for (Item item : event.getItems()) {
-                    player.getWorld().dropItemNaturally(player.getLocation(), item.getItemStack());
+                    player.getWorld().dropItemNaturally(block.getLocation(), item.getItemStack());
                 }
             }
 
