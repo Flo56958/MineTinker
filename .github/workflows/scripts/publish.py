1d53ecd839ed7612fd0ce0167d44cab9f320e73f
Flo56958 <florian.gutsche@fau.de>
2020-06-15 12:21:41 +0200

    Update Github Action

--- .github/workflows/scripts/publish.py
+++ .github/workflows/scripts/publish.py
@@ -1,22 +1,20 @@
 import sys
-import requests
 from datetime import datetime
 
-#Parameter syntax
-#0: script name
-#1: Target file
-#2: Published starting file name
-#3: Discord-Webhook-Address
-#4: Commit-SHA
+import requests
+
+# Parameter syntax
+# 0: script name
+# 1: Target file
+# 2: Published starting file name
+# 3: Discord-Webhook-Address
+# 4: Commit-SHA
 
 data = {}
 
-data['content'] = ' ' # <- What will be written in the chat before the file
+data['content'] = ' '  # <- What will be written in the chat before the file
 print(data, flush=True)
 
-result = requests.post(sys.argv[3],
-                        data=data, 
-                        files={'upload_file': (sys.argv[2] + "_" + str(datetime.now().strftime('%Y-%m-%d_%H-%M')) + '_commit_' + sys.argv[4][:6] + '.jar',
-                        open(sys.argv[1], "rb").read())})
+result = requests.post(sys.argv[3], data=data, files={'upload_file': (sys.argv[2] + "_" + str(datetime.now().strftime('%Y-%m-%d_%H-%M')) + '_commit_' + sys.argv[4][:6] + '.jar', open(sys.argv[1], "rb").read())})
 print(result, flush=True)
 print(result.content, flush=True)
